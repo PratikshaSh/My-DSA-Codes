eed221d358bee6453c2c7933c09c187eab76aeb3
PratikshaSh <pratiksha8520@gmail.com>
2021-02-18 18:03:46 +0530

    add union by rank

--- graph/GRAPH/cycleDetectionUndirectedDSU.cpp
+++ graph/GRAPH/cycleDetectionUndirectedDSU.cpp
@@ -1,3 +1,5 @@
+//O(n2)-->O(logn)-->o(1)
+
 //cycle Detection in an undirected graph using DSU
 #include<iostream>
 #include<list>
@@ -26,12 +28,19 @@ class Graph{
     }
 
     //Union
-    void union_set(int x, int y, int parent[]){
+    void union_set(int x, int y, int parent[], int rank[]){
         int s1 = findSet(x, parent);
         int s2 = findSet(y, parent);
 
         if(s1!=s2){
-            parent[s1] = s2;
+          if(rank[s1]<rank[s2]){
+              parent[s1]=s2;
+              rank[s2] += rank[s1];
+          }
+          else{
+              parent[s2]=s1;
+              rank[s1] += rank[s2];
+          }
         
         }
     } 
@@ -40,8 +49,10 @@ class Graph{
     bool contains_cycle(){
         //DSU logic to check if graph contains cycle or not
         int *parent = new int[V];
+        int *rank = new int[V];
         for(int i=0;i<V;i++){
             parent[i] = -1;
+            rank[i] = 1;
         }
 
         //iterate over the edge list
@@ -53,10 +64,10 @@ class Graph{
             int s2 = findSet(j, parent);
 
             if(s1 != s2){
-               union_set(s1,s2,parent); 
+               union_set(s1,s2,parent,rank); 
             }
             else{
-                cout<<"same parents"<<s1<<" and"<<s2<<endl;
+               // cout<<"same parents"<<s1<<" and"<<s2<<endl;
                 return true;
             }
         }
@@ -72,7 +83,7 @@ int main(){
     g.addEdge(1,2);
     g.addEdge(2,3);
     g.addEdge(3,0);
-    g.addEdge(0,1);
+//    g.addEdge(0,1);
 
   cout<< g.contains_cycle()<<endl;
    
