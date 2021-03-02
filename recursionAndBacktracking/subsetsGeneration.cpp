493d2317be7a7bf8198a967d825e142748ef6c09
PratikshaSh <pratiksha8520@gmail.com>
2021-03-02 18:17:43 +0530

    push code

---
+++ recursionAndBacktracking/subsetsGeneration.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void generate_subsequence(char *in, char *out, int i, int j){
+    if(in[i]=='\0'){
+        out[j]='\0';
+        cout<< out<<endl;
+        return;
+    }
+    //rec case
+    ///include the current char
+    out[j] = in[i];
+    generate_subsequence(in, out,i+1, j+1);
+    //exclude
+    generate_subsequence(in,out, i+1, j);
+}
+
+int main(){
+
+    char input[] = "abc";
+    char output[10];
+    generate_subsequence(input, output, 0, 0);
+}
