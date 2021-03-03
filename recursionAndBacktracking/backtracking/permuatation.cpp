104b8ca44cf2670464296068582996fbf70ff046
PratikshaSh <pratiksha8520@gmail.com>
2021-03-03 19:12:20 +0530

    for unique permuatations - use set

--- recursionAndBacktracking/backtracking/permuatation.cpp
+++ recursionAndBacktracking/backtracking/permuatation.cpp
@@ -1,18 +1,23 @@
 #include<iostream>
+#include<set>
+#include<string>
 using namespace std;
 
 //given a string s, task is to find all permutations of the string s
 
-void permute(char* in, int i){
+void permute(char* in, int i, set<string> &s){
     //base case
     if(in[i] =='\0'){
-        cout<<in<<endl;
+        //cout<<in<<endl;
+        string t(in);
+        s.insert(t);
+
         return;
     }
     //Rec case
     for(int j=i; in[j]!='\0';j++){
         swap(in[i],in[j]);
-        permute(in, i+1);
+        permute(in, i+1,s);
         //Backtracking-restore the original array
         swap(in[i], in[j]);
     }
@@ -22,8 +27,12 @@ void permute(char* in, int i){
 int main(){
     char in[100];
     cin>>in;
+    set<string> s;
+    permute(in, 0,s);
 
-    permute(in, 0);
+    for(auto str:s){
+        cout<<str<<",";
+    }
 
     return 0;
 }
