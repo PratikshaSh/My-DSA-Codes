db3721d2c63f668cf53e92eb289101ef0dd0e3f4
PratikshaSh <pratiksha8520@gmail.com>
2021-05-28 15:34:24 +0530

    dp

--- recursionAndBacktracking/backtracking/practice/subsetSum.cpp
+++ recursionAndBacktracking/backtracking/practice/subsetSum.cpp
@@ -16,6 +16,29 @@ public:
         }
         return isSS(sum-set[n-1], set, n-1) or isSS(sum, set, n-1);
     }
+    bool isSS2(int sum, int set[], int n){
+        bool dp[n+1][sum+1];
+        for(int i=0; i<=n; i++){
+            dp[i][0]= true;
+        }
+        for(int i=0; i<=sum; i++){
+            dp[0][i] = false;
+        
+        }
+
+        for(int i=1; i<= sum; i++){
+            for(int j=1; j<=sum; j++){
+                if(j<set[i-1]){
+                    dp[i][j] = dp[i-1][j];
+                }
+                if(j>= set[i-1]){
+
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-set[i-1]];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
 }ss;
 
 int main(){
